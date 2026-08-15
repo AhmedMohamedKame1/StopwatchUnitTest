dd1ff898ee163257911cfb32f577641212bbac46
AhmedMohamedKame1 <ahmed.mohamedmed1111@gmail.com>
2026-08-15 14:09:08 +0300

    tc#1: increment minutes nothing incremented

--- src/main/java/stopwatch/StopWatch.java
+++ src/main/java/stopwatch/StopWatch.java
@@ -1,5 +1,13 @@
 package stopwatch;
 
 public class StopWatch {
-   
+   int minutes;
+
+   public void increment(int min){
+    this.minutes = min;
+   }
+
+   public int getMinutes(){
+    return this.minutes;
+   }
 }
--- src/test/java/stopwatch/StopWatchTest.java
+++ src/test/java/stopwatch/StopWatchTest.java
@@ -1,5 +1,14 @@
 package stopwatch;
 
-public class StopWatchTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
+public class StopWatchTest {
+    @Test
+    public void givenMultiplePositiveMinutes_whenIncrement_thenMinutesIncrease(){
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.increment(10);
+        stopWatch.increment(20);
+        assertEquals(30, stopWatch.getMinutes());
+    }
 }
